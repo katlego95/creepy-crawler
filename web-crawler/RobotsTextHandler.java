f426889e073e5327f27642a0e8e86b9432094463
kaltego95 <katlegobeta@gmail.com>
2024-08-21 07:57:18 +0200

    Created RobotsTextHandler.java file. This is a class that handles robots.txt file of given domain

---
+++ web-crawler/RobotsTextHandler.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URL;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Handle reading and processing the robots.txt file
+ * of a given domain. It checks whether a URL is allowed
+ * based on the rules specified in the robots.txt file.
+ */
+public class RobotsTextHandler {
+
+
+}
